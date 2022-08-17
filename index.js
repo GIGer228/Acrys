7839f391c9cea72859a2ab4a1c111b03d5cc31d1
M[oo]N_R[av]E <60959458+GIGer228@users.noreply.github.com>
2022-08-17 15:12:21 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Require the necessary discord.js classes
+const {Client, GatewayIntentBits} = require('discord.js');
+const {token} = require('./config.json');
+//Create a new client instance
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+//When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Acrys client is ready!');
+});
+
+//Login to Discord with your client's token
+client.login(token);
