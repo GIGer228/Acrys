37f5d5a881594294392856473cdfc2a8a1bd4423
M[oo]N_R[av]E <60959458+GIGer228@users.noreply.github.com>
2022-08-17 16:14:47 +0200

    Added basics reply-commands
    Such as Ping-Pong!, Server/User info

--- index.js
+++ index.js
@@ -8,5 +8,25 @@ client.once('ready', () => {
     console.log('Acrys client is ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const  {commandName} = interaction;
+
+    if (commandName === 'ping'){
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server'){
+        await interaction.reply(`Server name: ${interaction.guild.name}\n` + 
+                                `Total members: ${interaction.guild.memberCount}\n` +
+                                `Created at : ${interaction.guild.createdAt}\n` +
+                                `Verification level: ${interaction.guild.verificationLevel}\n`);
+    } else if (commandName === 'user'){
+        await interaction.reply(`Username: ${interaction.user.username}\n` +
+                                `User tag: ${interaction.user.tag}\n` + 
+                                `User id: ${interaction.user.id}\n` + 
+                                `Joined at: ${interaction.user.createdAt}`);
+    }
+});
+
 //Login to Discord with your client's token
 client.login(token);
