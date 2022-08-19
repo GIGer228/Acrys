16825e97260d95a63995a257a03468aad044a0d2
M[oo]N_R[av]E <60959458+GIGer228@users.noreply.github.com>
2022-08-19 18:02:46 +0200

    Refactor Command List
    To create a command create a new file in "/commands" folder

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 //Require the necessary discord.js classes
 const {Client, GatewayIntentBits} = require('discord.js');
+
+const fs = require('node:js');
+const path = require('node:path');
+const {Client, Collection, GatewayIntentBits} = require('discord.js');
 const {token} = require('./config.json');
+
 //Create a new client instance
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
 //When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Acrys client is ready!');
@@ -11,20 +30,15 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    const  {commandName} = interaction;
+    const command = client.commands.get(interaction.commandName);
 
-    if (commandName === 'ping'){
-        await interaction.reply('Pong!');
-    } else if (commandName === 'server'){
-        await interaction.reply(`Server name: ${interaction.guild.name}\n` + 
-                                `Total members: ${interaction.guild.memberCount}\n` +
-                                `Created at : ${interaction.guild.createdAt}\n` +
-                                `Verification level: ${interaction.guild.verificationLevel}\n`);
-    } else if (commandName === 'user'){
-        await interaction.reply(`Username: ${interaction.user.username}\n` +
-                                `User tag: ${interaction.user.tag}\n` + 
-                                `User id: ${interaction.user.id}\n` + 
-                                `Joined at: ${interaction.user.createdAt}`);
+    if(!command) return;
+
+    try{
+        await command.execute(interaction);
+    }catch(error){
+        console.log(error);
+        await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
     }
 });
 
